f973b340f98001fcd744037348e2daa73d2e6e79
Jaya DeHart <dehartj97@gmail.com>
2022-02-04 11:12:38 -0800

    added custom content

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -4,6 +4,7 @@ export default function ExamplePage() {
       <div className="container-fluid">
         <h1>Example Page</h1>
         <p>Hello world!</p>
+        <p>Custom content!!!</p>
       </div>
     </main>
   );
